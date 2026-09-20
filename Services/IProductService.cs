8c978a9e5f59b8025623536e09e21dbc08f70855
Zawad <zawad5954@gmail.com>
2026-09-20 14:56:39 +0300

    progress

--- Services/IProductService.cs
+++ Services/IProductService.cs
@@ -5,9 +5,29 @@ namespace ProductManagementSystem.Services
     public interface IProductService
     {
         Task<List<Product>> GetProductsAsync(
-            string userId,
-            string? search,
-            string? sortOrder);
+    string userId,
+    string? search,
+    string? sortOrder,
+    int? categoryId,
+    int? brandId,
+    int? supplierId,
+    decimal? minPrice,
+    decimal? maxPrice,
+    int? minQuantity,
+    int? maxQuantity,
+    int page,
+    int pageSize);
+
+        Task<int> GetProductCountAsync(
+    string userId,
+    string? search,
+    int? categoryId,
+    int? brandId,
+    int? supplierId,
+    decimal? minPrice,
+    decimal? maxPrice,
+    int? minQuantity,
+    int? maxQuantity);
 
         Task<Product?> GetByIdAsync(int id, string userId);
         Task AddAsync(Product product);
