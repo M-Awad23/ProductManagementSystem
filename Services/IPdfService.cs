1c6b9cc7030c74626cdb60f8355d7528da1b4244
Mohammed Awad <137917013+M-Awad23@users.noreply.github.com>
2026-09-22 00:55:48 +0300

    Add full product PDF report operation

--- Services/IPdfService.cs
+++ Services/IPdfService.cs
@@ -5,5 +5,6 @@ namespace ProductManagementSystem.Services
     public interface IPdfService
     {
         byte[] GenerateProductPdf(Product product);
+        byte[] GenerateProductsPdf(IEnumerable<Product> products);
     }
 }
