8e74f818a868c0c4d476cacc97e17b74c2af0318
Zawad <zawad5954@gmail.com>
2026-09-21 14:14:48 +0300

    pdf di

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ using ProductManagementSystem.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+QuestPDF.Settings.License = QuestPDF.Infrastructure.LicenseType.Evaluation;
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
