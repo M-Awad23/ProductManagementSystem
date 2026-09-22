c507dca1d6eff996fb554da9985e62ad1c34df70
Mohammed Awad <137917013+M-Awad23@users.noreply.github.com>
2026-09-22 14:11:36 +0300

    Add user ownership to catalog entities

--- Services/TagService.cs
+++ Services/TagService.cs
@@ -1,4 +1,4 @@
-﻿using ProductManagementSystem.Models;
+using ProductManagementSystem.Models;
 using ProductManagementSystem.Repos;
 
 namespace ProductManagementSystem.Services
@@ -6,35 +6,11 @@ namespace ProductManagementSystem.Services
     public class TagService : ITagService
     {
         private readonly ITagRepository _tagRepository;
-
-        public TagService(ITagRepository tagRepository)
-        {
-            _tagRepository = tagRepository;
-        }
-
-        public IEnumerable<Tag> GetAllTags()
-        {
-            return _tagRepository.GetAllTags();
-        }
-
-        public Tag? GetTagById(int id)
-        {
-            return _tagRepository.GetTagById(id);
-        }
-
-        public void AddTag(Tag tag)
-        {
-            _tagRepository.AddTag(tag);
-        }
-
-        public void UpdateTag(Tag tag)
-        {
-            _tagRepository.UpdateTag(tag);
-        }
-
-        public void DeleteTag(int id)
-        {
-            _tagRepository.DeleteTag(id);
-        }
+        public TagService(ITagRepository tagRepository) => _tagRepository = tagRepository;
+        public IEnumerable<Tag> GetAllTags(string userId) => _tagRepository.GetAllTags(userId);
+        public Tag? GetTagById(int id, string userId) => _tagRepository.GetTagById(id, userId);
+        public void AddTag(Tag tag) => _tagRepository.AddTag(tag);
+        public void UpdateTag(Tag tag, string userId) => _tagRepository.UpdateTag(tag, userId);
+        public void DeleteTag(int id, string userId) => _tagRepository.DeleteTag(id, userId);
     }
 }
