06b9727782223d6a3238e8f3b7c4c1a3a8d5b9c8
Mohammed Awad <137917013+M-Awad23@users.noreply.github.com>
2026-09-22 14:15:19 +0300

    Complete catalog ownership contracts

--- Repos/ICategoryRepository.cs
+++ Repos/ICategoryRepository.cs
@@ -1,13 +1,13 @@
-﻿using ProductManagementSystem.Models;
+using ProductManagementSystem.Models;
 
 namespace ProductManagementSystem.Repos
 {
     public interface ICategoryRepository
     {
-        IEnumerable<Category> GetAllCategories();
-        Category? GetCategoryById(int id);
+        IEnumerable<Category> GetAllCategories(string userId);
+        Category? GetCategoryById(int id, string userId);
         void AddCategory(Category category);
-        void UpdateCategory(Category category);
-        void DeleteCategory(int id);
+        void UpdateCategory(Category category, string userId);
+        void DeleteCategory(int id, string userId);
     }
 }
