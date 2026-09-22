320920c17edbc9280b4920cc89f313db6cccc9e3
Mohammed Awad <137917013+M-Awad23@users.noreply.github.com>
2026-09-22 14:11:11 +0300

    Add user ownership to catalog entities

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,12 +1,11 @@
-﻿namespace ProductManagementSystem.Models
+namespace ProductManagementSystem.Models
 {
     public class Tag
     {
         public int Id { get; set; }
-
         public string Name { get; set; }
-
-        public ICollection<ProductTag> ProductTags { get; set; }
-            = new List<ProductTag>();
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public ICollection<ProductTag> ProductTags { get; set; } = new List<ProductTag>();
     }
 }
