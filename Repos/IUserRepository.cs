5e9074be22290a0880a1125a6293272eb6272dbb
Zawad <zawad5954@gmail.com>
2026-09-22 09:41:17 +0300

    progress

---
+++ Repos/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using ProductManagementSystem.Models;
+
+namespace ProductManagementSystem.Repos
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(string userId);
+        Task<User?> GetByEmailAsync(string email);
+        Task UpdateAsync(User user);
+    }
+}
