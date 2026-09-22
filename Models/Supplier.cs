2e114acf4c86641d64387190f972101a3ccce6b9
Mohammed Awad <137917013+M-Awad23@users.noreply.github.com>
2026-09-22 14:13:18 +0300

    Restore catalog model contracts safely

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -1,12 +1,13 @@
-namespace ProductManagementSystem.Models
+﻿namespace ProductManagementSystem.Models
 {
     public class Supplier
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Country { get; set; }
-        public string UserId { get; set; }
-        public User User { get; set; }
-        public ICollection<Product> Products { get; set; } = new List<Product>();
+
+        public ICollection<Product> Products { get; set; }
+           = new List<Product>();
+
     }
 }
