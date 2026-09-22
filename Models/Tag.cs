d055ab9535eabe22821bde5056ff66308fa65d29
Mohammed Awad <137917013+M-Awad23@users.noreply.github.com>
2026-09-22 14:13:20 +0300

    Restore catalog model contracts safely

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,11 +1,12 @@
-namespace ProductManagementSystem.Models
+﻿namespace ProductManagementSystem.Models
 {
     public class Tag
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
-        public string UserId { get; set; }
-        public User User { get; set; }
-        public ICollection<ProductTag> ProductTags { get; set; } = new List<ProductTag>();
+
+        public ICollection<ProductTag> ProductTags { get; set; }
+            = new List<ProductTag>();
     }
 }
