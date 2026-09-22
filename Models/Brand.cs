bd325eca31c0df62bfe5cec52c3dcb8c562be3f9
Mohammed Awad <137917013+M-Awad23@users.noreply.github.com>
2026-09-22 14:13:15 +0300

    Restore catalog model contracts safely

--- Models/Brand.cs
+++ Models/Brand.cs
@@ -1,11 +1,12 @@
-namespace ProductManagementSystem.Models
+﻿namespace ProductManagementSystem.Models
 {
     public class Brand
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string UserId { get; set; }
-        public User User { get; set; }
-        public ICollection<Product> Products { get; set; } = new List<Product>();
+
+        public ICollection<Product> Products { get; set; }
+           = new List<Product>();
+
     }
 }
