0f68300611b041ebb89fdfdf7b1bb166d029fb60
Mohammed Awad <137917013+M-Awad23@users.noreply.github.com>
2026-09-22 14:15:47 +0300

    Complete catalog ownership contracts

--- Models/Brand.cs
+++ Models/Brand.cs
@@ -6,7 +6,6 @@ namespace ProductManagementSystem.Models
         public string Name { get; set; }
         public string UserId { get; set; }
         public User User { get; set; }
-
         public ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
